652daa6e3b958ebb05344dff4a4ee64d34e5d40b
leechau <2940675@163.com>
2019-05-07 09:15:41 +0800

    modified

--- digital_all.py
+++ digital_all.py
@@ -14,6 +14,33 @@ def get_page_max(url):
     page_max = int(re.findall('/(\d+)/$', max_page_url)[0])
     return page_max
 
+def get_item(url):
+    html = requests.get(url, headers=headers)
+    soup = BeautifulSoup(html, 'lxml')
+    post = soup.find(attrs={'id': 'content'})
+    title = post.find(attrs={'class': 'title'}).string.strip()
+    if post.find(attrs={'id': 'syndication_permalink'}):
+        origin_url = post.find(attrs={'id': 'syndication_permalink'}).a['href']  #ok
+    else:
+        origin_url = ''
+    collect_date = post.find_all(attrs={'class': 'meta'})[-1].div.string.strip().split(',')[0]  #ok
+    collect_time = post.find_all(attrs={'class': 'meta'})[-1].div.string.strip().split(',')[1].strip()  #ok
+    categories = post.find(attrs={'class': 'category'})
+    category = ''
+    for itemm in categories.find_all('a'):
+        category = category + itemm.string + ','
+    category = category.strip(',')
+    tags = post.find(attrs={'class': 'tags'}).find_all(attrs={'rel': 'tag'})
+    tag = ''  #ok
+    for item in tags:
+        tag = tag + item.string + ','
+    tag = tag.strip(',')
+    entry_list = post.find(attrs={'class': 'entry'}).find_all('p')
+    entry = ''  #ok
+    for single in entry_list:
+        if single.string:
+            entry = entry + single.string + '\n'
+    entry = entry.replace("'", '"')
 
 
 
@@ -24,6 +51,10 @@ def get_page_max(url):
 
 if __name__ == '__main__':
     index_url = "https://chinadigitaltimes.net/chinese/%e6%9b%b4%e5%a4%9a%e6%96%87%e7%ab%a0/"
+    page_max = get_page_max(index_url)
+    print(page_max)
+    # for i in range(1, page_max + 1):
+    #     print('*')
 
 
 
